09b2e9277a6f81298e951efbe91cf9c72b608956
leon123858 <56253942+leon123858@users.noreply.github.com>
2021-04-23 12:21:43 +0800

    debugForReturnBuffer
    change Phin To NodeFetch

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config({
+	path: `${__dirname}/.env`,
+});
+
 import { Country, ServiceTypeTW, Lalamove, SpecialRequestTW } from '../src';
 import addMinutes from 'date-fns/fp/addMinutes';
 
 const lalamove = new Lalamove({
-	baseUrl: 'https://sandbox-rest.lalamove.com',
-	apiKey: 'c261c1d23a3749ca9111699b55c6139b',
-	apiSecret: 'MCwCAQACBQDqKHyrAgMBAAECBQDpfV3BAgMA96UCAwDyDwICE+UCAgqTAgIA',
+	baseUrl: process.env.LALAMOVE_BASE_URL || '',
+	apiKey: process.env.LALAMOVE_API_KEY || '',
+	apiSecret: process.env.LALAMOVE_API_SECRET || '',
 	country: Country.TW,
 });
 
